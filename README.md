a4ae807b9cefe746a9242d6ce5ceb8644fbbf5ae
Alex Schwartz <aschwartz@detaso.com>
2021-01-14 14:51:41 -0600

    Setup repo structure, test helpers, misc

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# enigma
+# Enigma project -- Final For Module 1
+[Project Page](https://backend.turing.io/module1/projects/enigma/)
---
+++ test/test_helper.rb
@@ -0,0 +1,8 @@
+require 'simplecov'
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'mocha/minitest'
+
+SimpleCov.start do
+  add_filter '/test/'
+end
