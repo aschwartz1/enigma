9cad23b204abdffb735ce73a74a8fac4b105ce58
Alex Schwartz <aschwartz@detaso.com>
2021-01-16 16:05:33 -0600

    Add comment organization to enigma, foreshadowing refactoring for encapsulation

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,47 +1,6 @@
 class Enigma
   SHIFT_KEYS = [:a, :b, :c, :d]
 
-  def encrypt(message, key, date)
-    # Need to generate a key?
-    # Need to generate a date?
-
-    # Calculate shifts
-    shift_rules = create_shifts(key, date)
-
-    # Encrypt the message
-    encrypted = do_encrypt(message, shift_rules)
-
-    # Format / return output hash
-    encryption_data(encrypted, key, date_string(date))
-  end
-
-  def encryption_data(encrypted_message, key, date)
-    {
-      encryption: encrypted_message,
-      key: key,
-      date: date
-    }
-  end
-
-  def do_encrypt(message, shift_rules)
-    encodings = calculate_encodings(shift_rules)
-
-    shift_index = -1
-    message.chars.map do |char|
-      shift_index = next_after(shift_index)
-      encodings[char][shift_index]
-    end.join('')
-  end
-
-  def calculate_encodings(shift_rules)
-    encodings = {}
-    character_set.each_with_index do |char, index|
-      encodings[char] = encodings_for(index, shift_rules)
-    end
-
-    encodings
-  end
-
   def create_shifts(key, date)
     return nil unless key.length == 5
 
@@ -76,6 +35,47 @@ class Enigma
     offsets
   end
 
+  ### --- ENCRYPT --- ###
+
+  def encrypt(message, key, date)
+    # Need to generate a key?
+    # Need to generate a date?
+
+    # Calculate shifts
+    shift_rules = create_shifts(key, date)
+
+    # Encrypt the message
+    encrypted = do_encrypt(message, shift_rules)
+
+    # Format / return output hash
+    encryption_data(encrypted, key, date_string(date))
+  end
+
+    def do_encrypt(message, shift_rules)
+    encodings = calculate_encodings(shift_rules)
+
+    shift_index = -1
+    message.chars.map do |char|
+      shift_index = next_after(shift_index)
+      encodings[char][shift_index]
+    end.join('')
+  end
+
+  def calculate_encodings(shift_rules)
+    encodings = {}
+    character_set.each_with_index do |char, index|
+      encodings[char] = encodings_for(index, shift_rules)
+    end
+
+    encodings
+  end
+
+  ### --- END ENCRYPT --- ###
+
+  ### --- DECRYPT --- ###
+
+  ### --- END DECRYPT --- ###
+
   private
 
   def character_set
@@ -92,6 +92,8 @@ class Enigma
   def date_string(date)
     date.strftime('%d%m%y')
   end
+
+  ### --- ENCRYPT --- ###
 
   def encodings_for(orig_index, shift_rules)
     encodings = []
@@ -116,4 +118,18 @@ class Enigma
       0
     end
   end
+
+  def encryption_data(encrypted_message, key, date)
+    {
+      encryption: encrypted_message,
+      key: key,
+      date: date
+    }
+  end
+
+  ### --- END ENCRYPT --- ###
+
+  ### --- DECRYPT --- ###
+
+  ### --- END DECRYPT --- ###
 end
