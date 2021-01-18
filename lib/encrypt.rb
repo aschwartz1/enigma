4591a37ac5ea27f485545df23f9ec1fc4fdba942
Alex Schwartz <aschwartz@detaso.com>
2021-01-18 14:06:03 -0600

    Add working runner files

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -3,27 +3,25 @@ require './lib/file_helper'
 require 'time'
 
 # Parse command line arguments
-abort('Encryption must be ran with BOTH an input and output file.') if ARGV.length != 2
-input_file = ARGV[0]
-output_file = ARGV[1]
+abort('Encryption must be ran with arguments `<original_file> <encrypted_file>`') if ARGV.length != 2
+original_file = ARGV[0]
+encrypted_file = ARGV[1]
 
 # Verify input file exists
 file_helper = FileHelper.new
-abort("#{input_file} doesn't exist.") unless file_helper.file_exists?(input_file)
+abort("#{original_file} doesn't exist.") unless file_helper.file_exists?(original_file)
 
 # Read in the message from it
-message = file_helper.read(input_file)
+message = file_helper.read(original_file)
 
 # Spin up an Enigma instance
 enigma = Enigma.new
 
 # Encrypt message
-encryption_data = enigma.encrypt(message, '02715', Date.new(1995, 8, 4))
+encryption_data = enigma.encrypt(message, '02715', '040895')
 
 # Write message to output file
-file_helper.write(encryption_data[:encryption], output_file)
+file_helper.write(encryption_data[:encryption], encrypted_file)
 
 # Print output message
-puts "Created '#{output_file}' with the key #{encryption_data[:key]} and date #{encryption_data[:date]}"
-
-# Profit
+puts "Created '#{encrypted_file}' with the key #{encryption_data[:key]} and date #{encryption_data[:date]}"
